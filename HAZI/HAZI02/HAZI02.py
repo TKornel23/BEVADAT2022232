2ccd900ad742f7fd231ec0ad7058d05bc0f11d0a
Kornél Tóth <tkornel23@gmail.com>
2023-03-12 19:09:05 +0100

    [HAZO02] Done

--- HAZI/HAZI02/HAZI02.py
+++ HAZI/HAZI02/HAZI02.py
@@ -19,7 +19,6 @@ def column_swap(input):
     array = np.array(input)
     array[:, [0, 1]] = array[:, [1, 0]]
     return array
-
 # %%
 # Készíts egy olyan függvényt ami összehasonlít két array-t és adjon vissza egy array-ben, hogy hol egyenlőek 
 # Pl Be: [7,8,9], [9,8,7] 
@@ -27,9 +26,10 @@ def column_swap(input):
 # compare_two_array()
 # egyenlő elemszámúakra kell csak hogy működjön
 def compare_two_array(array_1, array_2):
-    return np.array_equal(array_1, array_2, True)
-
-
+    arr1 = np.array(array_1)
+    arr2 = np.array(array_2)
+    result = np.where(arr1[::] == arr2[::])
+    return np.array(result)
 # %%
 # Készíts egy olyan függvényt, ami vissza adja string-ként a megadott array dimenzióit:
 # Be: [[1,2,3], [4,5,6]]
@@ -37,8 +37,12 @@ def compare_two_array(array_1, array_2):
 # get_array_shape()
 # 3D-vel még műküdnie kell!, 
 def get_array_shape(input):
-    array = np.array(input)
-    return f"sor: {array.shape[0]}, oszlop: {array.shape[1]}, melyseg: {array.shape[2]}"
+    arr = np.array(input)
+    shape = arr.shape
+    while len(shape) < 3:
+        shape = (1,) + shape
+    temp = shape[:3]
+    return f"sor: {temp[1]}, oszlop: {temp[2]}, melyseg: {temp[0]}"
 # %%
 # Készíts egy olyan függvényt, aminek segítségével elő tudod állítani egy neurális hálózat tanításához szükséges pred-et egy numpy array-ből. 
 # Bementként add meg az array-t, illetve hogy mennyi class-od van. Kimenetként pedig adjon vissza egy 2d array-t, ahol a sorok az egyes elemek. Minden nullákkal teli legyen és csak ott álljon egyes, ahol a bementi tömb megjelöli. 
@@ -47,12 +51,25 @@ def get_array_shape(input):
 # Ki: [[0,1,0,0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
 # encode_Y()
 
+def encode_Y(input, classes):
+    outarray = []
+    for x in input:
+        row = [0] * classes
+        row[x] = 1
+        outarray.append(row)
+    return np.array(outarray)
 # %%
 # A fenti feladatnak valósítsd meg a kiértékelését. Adj meg a 2d array-t és adj vissza a decodolt változatát
 # Be:  [[0,1,0,0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
 # Ki:  [1, 2, 0, 3]
 # decode_Y()
-
+def decore_Y(input):
+    outarray = []
+    arr = np.array(input)
+    temp = arr.tolist()
+    for x in temp:
+        outarray.append(x.index(1))
+    return np.array(outarray)
 # %%
 # Készíts egy olyan függvényt, ami képes kiértékelni egy neurális háló eredményét! Bemenetként egy listát és egy array-t és adja vissza azt az elemet, aminek a legnagyobb a valószínüsége(értéke) a listából.
 # Be: ['alma', 'körte', 'szilva'], [0.2, 0.2, 0.6]. # Az ['alma', 'körte', 'szilva'] egy lista!
@@ -61,16 +78,15 @@ def get_array_shape(input):
 def eval_classification(classes, preds):
     max = np.argmax(np.array(preds))
     return classes[max]
-
 # %%
 # Készíts egy olyan függvényt, ahol az 1D array-ben a páratlan számokat -1-re cseréli
 # Be: [1,2,3,4,5,6]
 # Ki: [-1,2,-1,4,-1,6]
 # repalce_odd_numbers()
 def repalce_odd_numbers(inputs):
-    array = np.array(inputs)
-    return map(lambda x: -1 if x % 2 == 0 else x, array)
-
+    arr = np.array(inputs)
+    arr[0::2] = -1
+    return arr
 # %%
 # Készíts egy olyan függvényt, ami egy array értékeit -1 és 1-re változtatja, attól függően, hogy az adott elem nagyobb vagy kisebb a paraméterként megadott számnál.
 # Ha a szám kisebb mint a megadott érték, akkor -1, ha nagyobb vagy egyenlő, akkor pedig 1.
@@ -79,8 +95,9 @@ def repalce_odd_numbers(inputs):
 # replace_by_value()
 def replace_by_value(input_list, a):
     array = np.array(input_list)
-    return map(lambda x: 1 if x >= a else -1, array)
-
+    array[array < a] = -1
+    array[array >= a] = 1
+    return array
 # %%
 # Készíts egy olyan függvényt, ami egy array értékeit összeszorozza és az eredményt visszaadja
 # Be: [1,2,3,4]
@@ -88,14 +105,16 @@ def replace_by_value(input_list, a):
 # array_multi()
 # Ha több dimenziós a tömb, akkor az egész tömb elemeinek szorzatával térjen vissza
 def array_multi(input):
-    array = np.array(input).flatten()
+    array = np.array(input)
     return np.prod(array)
 # %%
 # Készíts egy olyan függvényt, ami egy 2D array értékeit összeszorozza és egy olyan array-el tér vissza, aminek az elemei a soroknak a szorzata
 # Be: [[1, 2], [3, 4]]
 # Ki: [2, 12]
 # array_multi_2d()
-
+def array_multi_2d(input):
+    array = np.array(input)
+    return np.prod(array, axis=1)
 # %%
 # Készíts egy olyan függvényt, amit egy meglévő numpy array-hez készít egy bordert nullásokkal. Bementként egy array-t várjon és kimenetként egy array jelenjen meg aminek van border-je
 # Be: [[1,2],[3,4]]
@@ -105,7 +124,6 @@ def array_multi(input):
 def add_border(input):
     array = np.array(input)
     return np.pad(array, pad_width=1, mode='constant', constant_values=0)
-
 # %%
 # A KÖTVETKEZŐ FELADATOKHOZ NÉZZÉTEK MEG A NUMPY DATA TYPE-JÁT!
 
@@ -115,17 +133,20 @@ def add_border(input):
 # Ki: ['2023-03-01', '2023-03-02', .. , '2023-03-31',]
 # list_days()
 
-
+def list_days(date1, date2):
+    return np.arange(date1, date2, dtype="M8[D]")
 # %%
 # Írj egy fügvényt ami vissza adja az aktuális dátumot az alábbi formában: YYYY-MM-DD. Térjen vissza egy 'numpy.datetime64' típussal.
 # Be:
 # Ki: 2017-03-24
-
-
+def today():
+    return np.datetime64('today')
 # %%
 # Írj egy olyan függvényt ami visszadja, hogy mennyi másodperc telt el 1970 január 01. 00:02:00 óta. Int-el térjen vissza
 # Be: 
 # Ki: másodpercben az idó, int-é kasztolva
 # sec_from_1970()
-
-
+def sec_from_1970():
+    x = np.datetime64('1970-01-01T00:02:00')
+    current = np.datetime64('now')
+    return int((current - x) / np.timedelta64(1, 's'))
