25ca96bd90bbfaeda7b194080473e9e8e4f8ec1d
Kornél Tóth <tkornel23@gmail.com>
2023-04-23 10:18:22 +0200

    hazi08

--- GYAK/GYAK08/LinearRegressionSkeleton.py
+++ GYAK/GYAK08/LinearRegressionSkeleton.py
@@ -7,43 +7,33 @@ import matplotlib.pyplot as plt
 
 class LinearRegression:
     def __init__(self, epochs: int = 1000, lr: float = 1e-3):
+        self.epochs = epochs
+        self.lr = lr
         self.m = 0
         self.c = 0
-        self.L = lr  # The learning Rate
-        self.epochs = epochs  # The number of iterations to perform gradient descent
+        self.losses = []
 
 
     def fit(self, X: np.array, y: np.array):
-        #self.X = X
         n = float(len(X)) # Number of elements in X
 
         # Performing Gradient Descent 
-        losses = []
+        
         for i in range(self.epochs): 
-            y_pred = self.m*X + self.c  # The current predicted value of Y
+            y_pred = self.m * X + self.c  # The current predicted value of Y
 
             residuals = y - y_pred
             loss = np.sum(residuals ** 2)
-            losses.append(loss)
+            self.losses.append(loss)
             D_m = (-2/n) * sum(X * residuals)  # Derivative wrt m
             D_c = (-2/n) * sum(residuals)  # Derivative wrt c
-            self.m = self.m - self.L * D_m  # Update m
-            self.c = self.c - self.L * D_c  # Update c
+            self.m = self.m - self.lr * D_m  # Update m
+            self.c = self.c - self.lr * D_c  # Update c
+        return self.losses
 
     def predict(self, X):
-        pred = []
-        for x in X:
-            y_pred = self.m*x + self.c
-            pred.append(y_pred)
-
-        self.y_pred = self.m*X + self.c
-        return pred
+        return self.m * X + self.c
     
     def evaluate(self, X, y):
         pred = self.predict(X)
         return np.mean((pred - y)**2)
-
-    def plotRes(self,X,Y):
-        plt.scatter(X, Y)
-        plt.plot([min(X), max(X)], [min(self.y_pred), max(self.y_pred)], color='red') # predicted
-        plt.show()
--- GYAK/GYAK08/LinearRegressionTest.py
+++ GYAK/GYAK08/LinearRegressionTest.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 from matplotlib import pyplot as plt
-lr = LinearRegression(epochs=5000)
+lr = LinearRegression(epochs=20000, lr=0.01)
 iris = load_iris()
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
 X = df['petal width (cm)'].values
@@ -16,5 +16,9 @@ Y = df['sepal length (cm)'].values
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 lr.fit(X_train,y_train)
-lr.predict(X_test)
-lr.plotRes(X_test,y_test)
+y_pred = lr.predict(X_test)
+
+plt.scatter(X_test, y_test)
+plt.plot([min(X_test), max(X_test)], [min(y_pred),
+         max(y_pred)], color='red')  # predicted
+plt.show()
